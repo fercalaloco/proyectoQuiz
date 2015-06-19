ce455db7816bcb693baddbb3f329223c9e2f0394
Fernando Calatayud <fercalab@gmail.com>
2015-06-19 09:11:42 +0200

    coment y sesiones

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,15 @@ var express = require('express');
 var router = express.Router();
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
+var sessionController = require('../controllers/session_controller');
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz', errors: []});
 });
 router.param('quizId', quizController.load);
+router.get('/login', sessionController.new);
+router.post('/login', sessionController.create);
+router.get('/login', sessionController.destroy);
 router.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
