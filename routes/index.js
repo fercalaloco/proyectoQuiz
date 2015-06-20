5a961545f1f5e97bbc1cb89d6e42d95f43694ec2
Fernando Calatayud <fercalab@gmail.com>
2015-06-20 08:01:44 +0200

    moderacion de comentarios

--- routes/index.js
+++ routes/index.js
@@ -8,19 +8,22 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz', errors: []});
 });
 router.param('quizId', quizController.load);
+router.param('commentId', commentController.load);
 router.get('/login', sessionController.new);
 router.post('/login', sessionController.create);
 router.get('/logout', sessionController.destroy);
 router.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
-router.get('/quizes/new' sessionController.loginRequired, quizController.new);
-router.post('/quizes/create' sessionController.loginRequired, quizController.create);
-router.get('/quizes/:quizId(\\d+)/edit' sessionController.loginRequired, quizController.edit);
-router.put('/quizes/:quizId(\\d+)' sessionController.loginRequired, quizController.update);
-router.delete('/quizes/:quizId(\\d+)' sessionController.loginRequired, quizController.destroy);
+router.get('/quizes/new', sessionController.loginRequired, quizController.new);
+router.post('/quizes/create', sessionController.loginRequired, quizController.create);
+router.get('/quizes/:quizId(\\d+)/edit', sessionController.loginRequired, quizController.edit);
+router.put('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizController.update);
+router.delete('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizController.destroy);
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
+router.put('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish',
+	sessionController.loginRequired, commentController.publish);
 
 router.get('/author', function(req, res) {
   res.render('author', { title: 'creditos' });
